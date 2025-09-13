4b22497a39498e8c5bf70c7e8c7204eb87c14ae8
Artem <usik887@ukr.net>
2025-09-13 20:49:50 +0300

    add dark theme

--- java.js
+++ java.js
@@ -4,8 +4,19 @@ const quotes = [
   'Ваш час обмежений, не витрачайте його, живучи чужим життям - Стів Джобс',
 ];
 
+const container = document.getElementById('container');
+const pointer = document.getElementById('pointer');
 const quote = document.getElementById('quote');
 const btn = document.getElementById('gen-btn');
+const body = document.body;
+const theme = document.getElementById('theme');
+
+pointer.addEventListener('click', () => {
+  pointer.classList.toggle('clicked');
+  container.classList.toggle('clicked');
+  body.classList.toggle('clicked');
+  theme.classList.toggle('clicked');
+});
 
 function generateRandomQuote() {
   const Index = Math.floor(Math.random() * quotes.length);
