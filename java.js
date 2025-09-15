2e391c78e05a487f3e71d8af6efd130bfd568c03
Artem <usik887@ukr.net>
2025-09-15 22:23:26 +0300

    added stars instead of button

--- java.js
+++ java.js
@@ -6,17 +6,83 @@ const quoteRan = document.getElementById('quote');
 const btn = document.getElementById('gen-btn');
 const body = document.body;
 const theme = document.getElementById('theme');
+const toggleFavorite = document.getElementById('toggle-favorite');
+const addedToFavorite = document.getElementById('added');
+const favoriteContainer = document.getElementById('favorite-container');
 
+let currentIndex;
+let currentQuote;
+
+//  Зміна день-ніч
 pointer.addEventListener('click', () => {
   pointer.classList.toggle('clicked');
   container.classList.toggle('clicked');
   body.classList.toggle('clicked');
   theme.classList.toggle('clicked');
+  btn.classList.toggle('clicked');
+  toggleFavorite.classList.toggle('clicked');
+  const favoriteCards = document.querySelectorAll('.favorite-card');
+  favoriteCards.forEach((card) => {
+    card.classList.toggle('clicked');
+  });
 });
 
+//  Генерація випадкової цитати
 function generateRandomQuote() {
-  const Index = Math.floor(Math.random() * quotes.length);
-  const { author, quote } = quotes[Index];
+  addedToFavorite.textContent = '';
+  currentIndex = Math.floor(Math.random() * quotes.length);
+  currentQuote = quotes[currentIndex];
+  const { quote, author } = currentQuote;
   quoteRan.innerHTML = `<em>"${quote}"</em><br>(${author})`;
+
+  //  Присвоєння значення властивості isFavorite
+  if (currentQuote.isFavorite === undefined) {
+    currentQuote.isFavorite = false;
+  }
+
+  toggleFavorite.style.backgroundImage = currentQuote.isFavorite
+    ? 'url(./images/1.png)'
+    : 'url(./images/3.png)';
+
+  console.log(quotes);
 }
+
 btn.addEventListener('click', generateRandomQuote);
+
+//  Зміна властивості isFavorite і зміна повідомлення
+function toggleFavoriteQuote() {
+  if (currentQuote === undefined) {
+    addedToFavorite.textContent = 'Please generate quote!';
+    return;
+  }
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+
+  addedToFavorite.textContent = currentQuote.isFavorite
+    ? 'Quote added!'
+    : 'Remove from favorities';
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement('div');
+    if (container.classList.contains('clicked')) {
+      favoriteCard.classList.add('favorite-card', 'clicked');
+    } else {
+      favoriteCard.classList.add('favorite-card');
+    }
+    const { quote, author } = currentQuote;
+    favoriteCard.innerHTML = `<em>"${quote}"</em><br>(${author})`;
+    favoriteContainer.append(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll('.favorite-card');
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(quoteRan.textContent)) {
+        card.remove();
+      }
+    });
+  }
+
+  toggleFavorite.style.backgroundImage = currentQuote.isFavorite
+    ? 'url(./images/1.png)'
+    : 'url(./images/3.png)';
+}
+
+toggleFavorite.addEventListener('click', toggleFavoriteQuote);
